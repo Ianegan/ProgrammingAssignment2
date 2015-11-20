78b7b88806433dd6987e598fdb0a55518507de82
unknown <ILYNGDO@PC033F57.ap.ds.monsanto.com>
2015-11-20 17:50:29 +0530

    IanProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  inversevalue <- NULL
+  set <- function(y) {
+    x <<- y
+    inversevalue <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) inversevalue <<- mean
+  getmean <- function() inversevalue
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 
-}
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  inversevalue <- x$getmean()
+  if(!is.null(inversevalue)) {
+    message("getting cached data")
+    return(inversevalue)
+  }
+  data <- x$get()
+  inversevalue <- solve(data, ...)
+  x$setmean(inversevalue)
+  inversevalue
         ## Return a matrix that is the inverse of 'x'
 }
+
